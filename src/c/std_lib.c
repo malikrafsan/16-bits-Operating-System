de174aa3bcc2b85e00dad029a0315c6521921b0f
hafidznrg <hafidznur.hn@gmail.com>
2022-02-26 14:31:56 +0700

    add std_lib implementation

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -1,3 +1,49 @@
 #include "header/std_lib.h"
 
 // Opsional : Tambahkan implementasi kode C
+int div(int a, int b){
+    return a/b;
+}
+
+int mod(int a, int n){
+    return a-n*(a/n);
+}
+
+void memcpy(byte *dest, byte *src, unsigned int n){
+    for(int i=0; i<n; i++){
+        dest[i] = src[i];
+    }
+}
+
+unsigned int strlen(char *string){
+    int i=0;
+    while(string[i]!='\0'){
+        i++;
+    }
+    return i;
+}
+
+bool strcmp(char *s1, char *s2){
+    if (strlen(s1) != strlen(s2)){
+        return false;
+    }
+
+    for(int i=0; i<strlen(s1); i++){
+        if (s1[i] != s2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+void strcpy(char *dst, char *src){
+    for(int i=0; i<strlen(src); i++){
+        dst[i] = src[i];
+    }
+}
+
+void clear(byte *ptr, unsigned int n){
+    for(int i=0; i<n; i++){
+        ptr[i] = 0;
+    }
+}
